7bca1964df457f36227369b353d562c6b1c24159
Juan <Juan@DESKTOP-GDHTT9E>
2018-08-14 14:59:40 +0200

    - Arreglados fallos de sincronización con el repositorio local.

--- Rareject - GUI/Rareject (GUI)/VentanaProcesos.h
+++ Rareject - GUI/Rareject (GUI)/VentanaProcesos.h
@@ -59,6 +59,8 @@ namespace RarejectGUI {
 			}
 		}
 	private: System::Windows::Forms::ListBox^  listaProcesos;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox1;
 
 	protected:
 
@@ -86,33 +88,61 @@ namespace RarejectGUI {
 		void InitializeComponent(void)
 		{
 			this->listaProcesos = (gcnew System::Windows::Forms::ListBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// listaProcesos
 			// 
 			this->listaProcesos->FormattingEnabled = true;
-			this->listaProcesos->Location = System::Drawing::Point(-1, -1);
+			this->listaProcesos->Location = System::Drawing::Point(26, 12);
 			this->listaProcesos->Name = L"listaProcesos";
-			this->listaProcesos->Size = System::Drawing::Size(286, 264);
+			this->listaProcesos->Size = System::Drawing::Size(221, 147);
 			this->listaProcesos->TabIndex = 0;
 			this->listaProcesos->SelectedIndexChanged += gcnew System::EventHandler(this, &VentanaProcesos::listaProcesos_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(69, 226);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(139, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"FINALIZAR";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &VentanaProcesos::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(26, 188);
+			this->textBox1->Name = L"txtPID_NOMBRE";
+			this->textBox1->Size = System::Drawing::Size(221, 20);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &VentanaProcesos::textBox1_TextChanged);
 			// 
 			// VentanaProcesos
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->listaProcesos);
 			this->Name = L"VentanaProcesos";
 			this->Text = L"VentanaProcesos";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 
 	private: System::Void listaProcesos_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
 	
+		textBox1->Text = listaProcesos->GetItemText(listaProcesos->SelectedItem);
 	
 	}
 
-	};
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+}
+};
 }
