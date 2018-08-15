f018146bdc3243f6094a50c0c05b35d8ec4f6439
Juan <Juan@DESKTOP-GDHTT9E>
2018-08-15 04:34:24 +0200

    - Agregada interfaz gráfica. - Ahora el PID se asigna al textbox de la Main form. - El menú evita fallos si el usuario intenta re-escalar la dimensión de la ventana.

--- Rareject - GUI/Rareject (GUI)/Rareject.h
+++ Rareject - GUI/Rareject (GUI)/Rareject.h
@@ -35,20 +35,21 @@ namespace RarejectGUI {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::Button^  btnProcesos;
 
+
+
+	private: System::Windows::Forms::Button^  btnProcesos;
 	private: System::Windows::Forms::Label^  label4;
 	private: System::Windows::Forms::Label^  label5;
 	private: System::Windows::Forms::TextBox^  txtRuta;
-
 	private: System::Windows::Forms::Button^  btnBuscar;
-
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::LinkLabel^  linkLabel1;
+	private: System::Windows::Forms::Button^  button2;
 
 	protected:
 
@@ -66,9 +67,6 @@ namespace RarejectGUI {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Rareject::typeid));
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->btnProcesos = (gcnew System::Windows::Forms::Button());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
@@ -77,67 +75,54 @@ namespace RarejectGUI {
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(165, 9);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(78, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Realizado por :";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(144, 33);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(121, 13);
-			this->label2->TabIndex = 1;
-			this->label2->Text = L"Juan Pablo Pereira Caro";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(138, 58);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(127, 13);
-			this->label3->TabIndex = 2;
-			this->label3->Text = L"Diego Rodríguez Vicente";
 			// 
 			// btnProcesos
 			// 
-			this->btnProcesos->Location = System::Drawing::Point(326, 102);
+			this->btnProcesos->BackColor = System::Drawing::Color::Black;
+			this->btnProcesos->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btnProcesos.BackgroundImage")));
+			this->btnProcesos->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->btnProcesos->ForeColor = System::Drawing::Color::Cyan;
+			this->btnProcesos->Location = System::Drawing::Point(30, 206);
 			this->btnProcesos->Name = L"btnProcesos";
-			this->btnProcesos->Size = System::Drawing::Size(75, 23);
+			this->btnProcesos->Size = System::Drawing::Size(114, 23);
 			this->btnProcesos->TabIndex = 3;
-			this->btnProcesos->Text = L"Procesos";
-			this->btnProcesos->UseVisualStyleBackColor = true;
+			this->btnProcesos->Text = L"Buscar proceso";
+			this->btnProcesos->UseVisualStyleBackColor = false;
 			this->btnProcesos->Click += gcnew System::EventHandler(this, &Rareject::btnProcesos_Click);
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(12, 102);
+			this->label4->BackColor = System::Drawing::SystemColors::MenuText;
+			this->label4->ForeColor = System::Drawing::Color::Cyan;
+			this->label4->Location = System::Drawing::Point(12, 131);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(31, 13);
+			this->label4->Size = System::Drawing::Size(37, 13);
 			this->label4->TabIndex = 4;
-			this->label4->Text = L"PID :";
+			this->label4->Text = L"- PID :";
+			this->label4->Click += gcnew System::EventHandler(this, &Rareject::label4_Click);
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(12, 143);
+			this->label5->BackColor = System::Drawing::SystemColors::MenuText;
+			this->label5->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label5->ForeColor = System::Drawing::Color::Cyan;
+			this->label5->Location = System::Drawing::Point(12, 97);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(62, 13);
+			this->label5->Size = System::Drawing::Size(65, 13);
 			this->label5->TabIndex = 5;
-			this->label5->Text = L"Ruta DLL : ";
+			this->label5->Text = L"- Ruta DLL: ";
 			// 
 			// txtRuta
 			// 
 			this->txtRuta->Enabled = false;
-			this->txtRuta->Location = System::Drawing::Point(80, 140);
+			this->txtRuta->Location = System::Drawing::Point(83, 94);
 			this->txtRuta->Name = L"txtRuta";
 			this->txtRuta->Size = System::Drawing::Size(204, 20);
 			this->txtRuta->TabIndex = 6;
@@ -145,42 +130,108 @@ namespace RarejectGUI {
 			// 
 			// btnBuscar
 			// 
-			this->btnBuscar->Location = System::Drawing::Point(326, 138);
+			this->btnBuscar->BackColor = System::Drawing::Color::Black;
+			this->btnBuscar->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btnBuscar.BackgroundImage")));
+			this->btnBuscar->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->btnBuscar->ForeColor = System::Drawing::Color::Cyan;
+			this->btnBuscar->Location = System::Drawing::Point(30, 177);
 			this->btnBuscar->Name = L"btnBuscar";
-			this->btnBuscar->Size = System::Drawing::Size(75, 23);
+			this->btnBuscar->Size = System::Drawing::Size(114, 23);
 			this->btnBuscar->TabIndex = 7;
-			this->btnBuscar->Text = L"Buscar";
-			this->btnBuscar->UseVisualStyleBackColor = true;
+			this->btnBuscar->Text = L"Buscar DLL";
+			this->btnBuscar->UseVisualStyleBackColor = false;
 			this->btnBuscar->Click += gcnew System::EventHandler(this, &Rareject::btnBuscar_Click);
 			// 
 			// openFileDialog1
 			// 
 			this->openFileDialog1->DefaultExt = L"dll";
 			this->openFileDialog1->Filter = L"Biblioteca dinámica (*.dll)|*.dll";
+			this->openFileDialog1->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &Rareject::openFileDialog1_FileOk);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(165, 232);
+			this->button3->BackColor = System::Drawing::Color::Transparent;
+			this->button3->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.BackgroundImage")));
+			this->button3->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button3->ForeColor = System::Drawing::Color::Cyan;
+			this->button3->Location = System::Drawing::Point(30, 235);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->Size = System::Drawing::Size(114, 23);
 			this->button3->TabIndex = 8;
-			this->button3->Text = L"Inyectar !";
-			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Text = L"Inyectar";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &Rareject::button3_Click);
 			// 
 			// textBox1
 			// 
 			this->textBox1->Enabled = false;
-			this->textBox1->Location = System::Drawing::Point(80, 102);
+			this->textBox1->Location = System::Drawing::Point(83, 128);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(46, 20);
 			this->textBox1->TabIndex = 9;
 			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
+			// button1
+			// 
+			this->button1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.BackgroundImage")));
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button1->ForeColor = System::Drawing::Color::Cyan;
+			this->button1->Location = System::Drawing::Point(152, 126);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(135, 23);
+			this->button1->TabIndex = 10;
+			this->button1->Text = L"Asignar proceso buscado";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Rareject::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::MenuText;
+			this->label1->ForeColor = System::Drawing::Color::Cyan;
+			this->label1->Location = System::Drawing::Point(12, 285);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 13);
+			this->label1->TabIndex = 11;
+			this->label1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->BackColor = System::Drawing::SystemColors::MenuText;
+			this->linkLabel1->DisabledLinkColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->linkLabel1->LinkColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->linkLabel1->Location = System::Drawing::Point(27, 285);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(250, 13);
+			this->linkLabel1->TabIndex = 12;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"https://github.com/JuanPabloPereiraCaro/Rareject";
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::MenuText;
+			this->button2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.Image")));
+			this->button2->Location = System::Drawing::Point(182, 184);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 67);
+			this->button2->TabIndex = 13;
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Rareject::button2_Click);
+			// 
 			// Rareject
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(424, 286);
+			this->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(300, 316);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->btnBuscar);
@@ -188,34 +239,64 @@ namespace RarejectGUI {
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->btnProcesos);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
+			this->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"Rareject";
-			this->Text = L"Rareject";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Rareject v 0.1";
 			this->Load += gcnew System::EventHandler(this, &Rareject::MyForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
+
 	#pragma endregion
+
+		VentanaProcesos ^ Vent_Proc;
+
 		private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+
+			Vent_Proc = gcnew VentanaProcesos();
+
 		}
 
 		private: System::Void btnProcesos_Click(System::Object^  sender, System::EventArgs^  e) {
 			
 			/* Se abre la ventana con los procesos activos en el sistema */
 
-			VentanaProcesos ^ Vent_Proc = gcnew VentanaProcesos();
 			Vent_Proc->ShowDialog();
 
 		}
 
 		private: System::Void btnBuscar_Click(System::Object^  sender, System::EventArgs^  e) {
+			
 			if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+		
 				txtRuta->Text = openFileDialog1->FileName;
+			
 			}
+		
 		}
+
+		private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+
+
+		}
+private: System::Void openFileDialog1_FileOk(System::Object^  sender, System::ComponentModel::CancelEventArgs^  e) {
+}
+private: System::Void label4_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	
+	textBox1->Text = System::Convert::ToString(processID);
+
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	this->Close();
+
+}
 };
 }
--- Rareject - GUI/Rareject (GUI)/VentanaProcesos.h
+++ Rareject - GUI/Rareject (GUI)/VentanaProcesos.h
@@ -88,6 +88,7 @@ namespace RarejectGUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(VentanaProcesos::typeid));
 			this->listaProcesos = (gcnew System::Windows::Forms::ListBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
@@ -97,6 +98,8 @@ namespace RarejectGUI {
 			// 
 			// listaProcesos
 			// 
+			this->listaProcesos->BackColor = System::Drawing::SystemColors::WindowText;
+			this->listaProcesos->ForeColor = System::Drawing::Color::Cyan;
 			this->listaProcesos->FormattingEnabled = true;
 			this->listaProcesos->Location = System::Drawing::Point(26, 22);
 			this->listaProcesos->Name = L"listaProcesos";
@@ -106,35 +109,40 @@ namespace RarejectGUI {
 			// 
 			// button1
 			// 
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button1->ForeColor = System::Drawing::Color::Cyan;
+			this->button1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.Image")));
 			this->button1->Location = System::Drawing::Point(69, 226);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(139, 23);
 			this->button1->TabIndex = 1;
-			this->button1->Text = L"FINALIZAR";
+			this->button1->Text = L"Finalizar";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &VentanaProcesos::button1_Click);
 			// 
 			// textBox1
 			// 
+			this->textBox1->Enabled = false;
 			this->textBox1->Location = System::Drawing::Point(26, 188);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(221, 20);
 			this->textBox1->TabIndex = 2;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &VentanaProcesos::textBox1_TextChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->ForeColor = System::Drawing::Color::Cyan;
 			this->label1->Location = System::Drawing::Point(23, 7);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(25, 13);
 			this->label1->TabIndex = 3;
 			this->label1->Text = L"PID";
-			this->label1->Click += gcnew System::EventHandler(this, &VentanaProcesos::label1_Click);
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->ForeColor = System::Drawing::Color::Cyan;
 			this->label2->Location = System::Drawing::Point(54, 7);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(102, 13);
@@ -145,32 +153,37 @@ namespace RarejectGUI {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->ClientSize = System::Drawing::Size(277, 261);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->listaProcesos);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"VentanaProcesos";
-			this->Text = L"VentanaProcesos";
+			this->Text = L"Procesos activos...";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 
+	/*public: static int processID;*/
+
 	private: System::Void listaProcesos_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
-		String^ process = listaProcesos->GetItemText(listaProcesos->SelectedItem); 
+
+		String^ process = listaProcesos->GetItemText(listaProcesos->SelectedItem);
 		array<String^>^ processinfo = process->Split(' ');
-		
+
 		textBox1->Text = processinfo[1]->Substring(1, (processinfo[1]->Length - 2)) + " (" + processinfo[0] + ")";
 		processID = System::Convert::ToInt32(processinfo[0]);
+
 	}
 
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+		this->Close();
+
 	}
-private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
-}
 };
 }
